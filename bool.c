99dc033be302ae3dbb13c2edee4414bb783d3bb9
Nasim <mdnasimhosen333@gmail.com>
2025-04-06 21:18:10 +0600

    learn c language

---
+++ bool.c
@@ -0,0 +1,8 @@
+#include<stdbool.h>
+int main()
+{
+    bool a = true;
+    bool b = false;
+    printf("a = %d b = %d", a, b);
+    return 0;
+}
---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    float b = 4.4;
+    char c = 'N';
+    printf("a = %d b = %.3f c = %c", a, b, c);
+    return 0;
+}
