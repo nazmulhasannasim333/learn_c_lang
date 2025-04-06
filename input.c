65b9722b97870b9a370d3293ee409f48387b5baf
Nasim <mdnasimhosen333@gmail.com>
2025-04-06 21:26:23 +0600

    learn int, float, char

--- input.c
+++ input.c
@@ -2,7 +2,13 @@
 int main()
 {
     int a;
+    float b;
+    char c;
     scanf("%d", &a);
+    scanf("%f", &b);
+    scanf(" %c", &c);
     printf("a = %d", a);
+    printf(" b = %.3f", b);
+    printf(" c = %c", c);
     return 0;
 }
